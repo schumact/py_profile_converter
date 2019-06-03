17efe3d37f579010448e97d614083b4dbbdfe0f9
unknown <schumact@mail.uc.edu>
2019-06-03 17:47:13 -0400

    Added country.py. Contains funcs and objects relating to countries and states

---
+++ classes/country.py
@@ -0,0 +1,171 @@
+COUNTRY_DICT = {
+    "United States": "US"
+}
+
+ABBREV_COUNTRY_DICT = {
+    "US": "United States"
+}
+
+ABBREV_STATE_DICT = {
+    'AK': 'Alaska',
+    'AL': 'Alabama',
+    'AR': 'Arkansas',
+    'AS': 'American Samoa',
+    'AZ': 'Arizona',
+    'CA': 'California',
+    'CO': 'Colorado',
+    'CT': 'Connecticut',
+    'DC': 'District of Columbia',
+    'DE': 'Delaware',
+    'FL': 'Florida',
+    'GA': 'Georgia',
+    'GU': 'Guam',
+    'HI': 'Hawaii',
+    'IA': 'Iowa',
+    'ID': 'Idaho',
+    'IL': 'Illinois',
+    'IN': 'Indiana',
+    'KS': 'Kansas',
+    'KY': 'Kentucky',
+    'LA': 'Louisiana',
+    'MA': 'Massachusetts',
+    'MD': 'Maryland',
+    'ME': 'Maine',
+    'MI': 'Michigan',
+    'MN': 'Minnesota',
+    'MO': 'Missouri',
+    'MP': 'Northern Mariana Islands',
+    'MS': 'Mississippi',
+    'MT': 'Montana',
+    'NA': 'National',
+    'NC': 'North Carolina',
+    'ND': 'North Dakota',
+    'NE': 'Nebraska',
+    'NH': 'New Hampshire',
+    'NJ': 'New Jersey',
+    'NM': 'New Mexico',
+    'NV': 'Nevada',
+    'NY': 'New York',
+    'OH': 'Ohio',
+    'OK': 'Oklahoma',
+    'OR': 'Oregon',
+    'PA': 'Pennsylvania',
+    'PR': 'Puerto Rico',
+    'RI': 'Rhode Island',
+    'SC': 'South Carolina',
+    'SD': 'South Dakota',
+    'TN': 'Tennessee',
+    'TX': 'Texas',
+    'UT': 'Utah',
+    'VA': 'Virginia',
+    'VI': 'Virgin Islands',
+    'VT': 'Vermont',
+    'WA': 'Washington',
+    'WI': 'Wisconsin',
+    'WV': 'West Virginia',
+    'WY': 'Wyoming'
+}
+
+STATE_DICT = {
+    'Alaska': 'AK',
+    'Alabama': 'AL',
+    'Arkansas': 'AR',
+    'American Samoa': 'AS',
+    'Arizona': 'AZ',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'District of Columbia': 'DC',
+    'Delaware': 'DE',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Guam': 'GU',
+    'Hawaii': 'HI',
+    'Iowa': 'IA',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Massachusetts': 'MA',
+    'Maryland': 'MD',
+    'Maine': 'ME',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Missouri': 'MO',
+    'Northern Mariana Islands': 'MP',
+    'Mississippi': 'MS',
+    'Montana': 'MT',
+    'National': 'NA',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Nebraska': 'NE',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'Nevada': 'NV',
+    'New York': 'NY',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Puerto Rico': 'PR',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Virginia': 'VA',
+    'Virgin Islands': 'VI',
+    'Vermont': 'VT',
+    'Washington': 'WA',
+    'Wisconsin': 'WI',
+    'West Virginia': 'WV',
+    'Wyoming': 'WY'
+}
+
+
+def abbreviate_state(state):
+    if len(state) > 2:
+        try:
+            state = STATE_DICT[state]
+            return state
+        except KeyError as e:
+            print(f"Couldn't find {state} in STATE_DICT")
+    else:
+        return state
+
+
+def full_form_state(state):
+    if len(state) == 2:
+        try:
+            state = ABBREV_STATE_DICT[state]
+            return state
+        except KeyError as e:
+            print(f"Couldn't find {state} in ABBREV_STATE_DICT")
+    else:
+        return state
+
+
+def abbreviate_country(country):
+    if len(country) > 2:
+        try:
+            country = COUNTRY_DICT[country]
+            return country
+        except KeyError as e:
+            print(f"Couldn't find {country} in COUNTRY_DICT")
+    else:
+        return country
+
+
+def full_form_country(country):
+    if len(country) == 2:
+        try:
+            country = ABBREV_COUNTRY_DICT[country]
+            return country
+        except KeyError as e:
+            print(f"Couldn't find {country} in ABBREV_COUNTRY_DICT")
+    else:
+        return country
