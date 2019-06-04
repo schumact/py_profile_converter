7a9153c640ebbb09755f63cdbdd836274c1d1936
unknown <schumact@mail.uc.edu>
2019-06-04 17:52:54 -0400

    added anb support. Added **kwargs to Billing constructor

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from sys import exit
 from classes.cyber import from_cyber, to_cyber
 from classes.pd import from_pd, to_pd
 from classes.phantom import from_phantom, to_phantom
+from classes.anb import from_anb, to_anb
 
 
 def arg_parser():
@@ -25,12 +26,14 @@ FROM_BOTS = {
     "cyber": from_cyber,
     "phantom": from_phantom,
     "pd": from_pd,
+    "anb": from_anb
 }
 
 TO_BOTS = {
     "cyber": to_cyber,
     "phantom": to_phantom,
     "pd": to_pd,
+    "anb": to_anb
 }
 
 
@@ -46,7 +49,7 @@ class Card:
 
 class Billing:
     def __init__(self, first=None, last=None, address_one=None, address_two=None,
-                 zipcode=None, city=None, country=None, state=None):
+                 zipcode=None, city=None, country=None, state=None, **kwargs):
         self.first = first
         self.last = last
         self.address_one = address_one
@@ -59,7 +62,7 @@ class Billing:
 
 class Shipping:
     def __init__(self, first=None, last=None, email=None, address_one=None, address_two=None,
-                 zipcode=None, city=None, country=None, state=None, same_as_del=None):
+                 zipcode=None, city=None, country=None, state=None, same_as_del=None, **kwargs):
         self.first = first
         self.last = last
         self.email = email
@@ -70,6 +73,7 @@ class Shipping:
         self.country = country
         self.state = state
         self.same_as_del = same_as_del
+        self.kwargs = kwargs
 
 
 class CommonFormat:
@@ -78,22 +82,29 @@ class CommonFormat:
         self.billing = Billing
         self.card = Card
         self.title = None
+        self.email = None
+        self.phone = None
+        self.limit = None
+        self.same_as_ship = None
 
 
-def read_from(file):
+def read_from_json(json_file):
     parsed = None
     try:
-        with open(file, 'r') as fi:
-            try:
-                parsed = json.load(fi)
-            except json.JSONDecodeError as e:
-                print(e)
-    except FileNotFoundError as e:
-        exit("Please supply profile json file that already exists.")
+        with open(json_file, 'r') as fi:
+            parsed = json.load(fi)
+    except (FileNotFoundError, json.JSONDecodeError) as e:
+        if json.JSONDecodeError:
+            exit("File does not contain valid Json. Exiting program")
+        else:
+            exit("Please supply profile json file that already exists.")
     return parsed
 
 
 def create_profiles_json_file(profiles):
+    if not args.new_file_name.endswith('json'):
+        exit(f"The file to be created, {args.new_file_name} ,"
+             f" was not of .json file type ")
     with open(args.new_file_name, 'w') as fi:
         json.dump(profiles, fi)
         print(f'Success! Profiles have been created in {args.new_file_name}')
@@ -103,8 +114,30 @@ def pretty_print_json(json_obj):
     return json.dumps(json_obj, indent=4, sort_keys=True)
 
 
+def generate_profiles(to_bot_func, old_profiles):
+    if to_bot_func == to_anb:
+        return to_bot(old_profiles, args.new_file_name)
+    else:
+        new_profiles = to_bot_func(old_profiles)
+        return create_profiles_json_file(new_profiles)
+
+
+def from_bot_to_common(from_bot_func):
+    if from_bot_func == from_anb:
+        return from_bot_func(args.file_, CommonFormat)
+    else:
+        return from_bot_func(read_from_json(args.file_), CommonFormat)
+
+
+def convert(from_bot_func, to_bot_func):
+    old_profiles = from_bot_to_common(from_bot_func)
+    generate_profiles(to_bot_func, old_profiles)
+
+
 if __name__ == '__main__':
     args = arg_parser()
+    if args.former.lower() == args.to.lower():
+        exit("-t and -f flags must be different. Cannot convert between the same bot")
     from_bot = None
     to_bot = None
     try:
@@ -113,6 +146,5 @@ if __name__ == '__main__':
     except KeyError as e:
         exit("Either the -f or -t option was passed an invalid value. "
              "Current valid options for both fields are cyber, pd, phantom")
-    old_profiles = from_bot(read_from(args.file_), CommonFormat)
-    new_profiles = to_bot(old_profiles)
-    create_profiles_json_file(new_profiles)
+    convert(from_bot, to_bot)
+
